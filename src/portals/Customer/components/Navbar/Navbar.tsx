938cccb9970e3e361b68714a025f922bd3de6d9a
VanshVg <vansh.vg18@gmail.com>
2025-05-05 19:28:19 +0530

    modified design for navbar

--- src/portals/Customer/components/Navbar/Navbar.tsx
+++ src/portals/Customer/components/Navbar/Navbar.tsx
@@ -1,57 +1,89 @@
 import { useState } from "react";
-import { logoPath } from "../../../../types/constants";
 import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
-import { navMenuElements } from "../../types/constants";
+import {
+  navMenuCornerElements,
+  navMenuMiddleElements,
+} from "../../types/constants";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav>
-      <div className="p-4 flex justify-between">
-        <div>
-          <img
-            src={logoPath}
-            className="h-[130px] w-[130px] md:h-[150px] md:w-[150px] cursor-pointer"
-          />
+    <nav className="text-[#2b2b2b]">
+      <div className="p-4 border-t-[1px] border-b-[1px] border-[#2b2b2b] flex justify-between">
+        <div className="md:mt-3 md:w-[200px]">Search...</div>
+        <div className="md:flex hidden md:box gap-4 mt-3 text-[18px]">
+          {navMenuMiddleElements.map((element, index) => {
+            return (
+              <div
+                className={`pr-4 max-h-[25px] ${
+                  index !== navMenuMiddleElements.length - 1
+                    ? "border-r-[1px]"
+                    : ""
+                }`}
+                key={element.label}
+              >
+                <h2
+                  key={element.label}
+                  className={`text-center cursor-pointer mx-auto ease-in duration-100 hover:text-[#707070]`}
+                >
+                  {element.label}
+                </h2>
+              </div>
+            );
+          })}
         </div>
-        <div></div>
-        <div>
-          <div className="md:hidden mt-10 ">
-            <AiOutlineMenu
-              color="#ff5715"
-              size={"25px"}
-              onClick={() => {
-                setIsOpen(true);
-              }}
-            />
-          </div>
+        <div className="md:flex hidden md:box gap-6 text-[18px] mt-3">
+          {navMenuCornerElements.map((element) => {
+            return (
+              <div
+                className="flex gap-2 max-h-[25px] cursor-pointer"
+                key={element.label}
+              >
+                <element.icon className="mt-[3px] text-[20px]" />
+                <h2 className="text-center ease-in duration-100 hover:text-[#707070]">
+                  {element.label}
+                </h2>
+              </div>
+            );
+          })}
+        </div>
+        <div className="md:hidden">
+          <AiOutlineMenu
+            color="#2b2b2b"
+            size={"25px"}
+            onClick={() => {
+              setIsOpen(true);
+            }}
+          />
         </div>
       </div>
       <div
-        className={`md:hidden w-full h-full bg-white fixed top-0 transition-opacity duration-500 ${
+        className={`md:hidden w-full h-full bg-[#fafafa] fixed top-0 transition-opacity duration-500 ${
           !isOpen ? "opacity-0 pointer-events-none" : "opacity-100"
         }`}
       >
         <AiOutlineClose
-          color="#ff5715"
+          color="#2b2b2b"
           size={"25px"}
           className="absolute right-4 top-14"
           onClick={() => {
             setIsOpen(false);
           }}
         />
-        <div className="mt-28 text-[#ff5715] text-[18px]">
-          {navMenuElements.map((element) => {
-            return (
-              <h2
-                key={element.label}
-                className="text-center mt-5 border-b-[1px] max-w-[250px] mx-auto pb-2"
-              >
-                {element.label}
-              </h2>
-            );
-          })}
+        <div className="mt-28 text-[18px]">
+          {[...navMenuMiddleElements, ...navMenuCornerElements].map(
+            (element) => {
+              return (
+                <div
+                  className="mt-5 border-b-[1px] max-w-[250px] mx-auto"
+                  key={element.label}
+                >
+                  <h2 className="text-center mx-auto pb-2">{element.label}</h2>
+                </div>
+              );
+            }
+          )}
         </div>
       </div>
     </nav>
--- src/portals/Customer/types/constants.ts
+++ src/portals/Customer/types/constants.ts
@@ -1,24 +1,35 @@
 import { ICustomerRoutes } from ".";
+import { AiOutlineUser } from "react-icons/ai";
+import { BsCartFill } from "react-icons/bs";
 
-export const navMenuElements = [
+export const navMenuMiddleElements = [
   {
     label: "Home",
     path: ICustomerRoutes.Home,
   },
   {
-    label: "Products",
+    label: "Shop",
+    path: ICustomerRoutes.Home,
+  },
+  {
+    label: "About Us",
     path: ICustomerRoutes.Home,
   },
   {
     label: "Orders",
     path: ICustomerRoutes.Home,
   },
+];
+
+export const navMenuCornerElements = [
   {
     label: "Cart",
     path: ICustomerRoutes.Home,
+    icon: BsCartFill,
   },
   {
     label: "Account",
     path: ICustomerRoutes.Home,
+    icon: AiOutlineUser,
   },
 ];
