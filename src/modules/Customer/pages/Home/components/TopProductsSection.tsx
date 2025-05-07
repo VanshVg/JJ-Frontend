9519d4de11302b2b8f0e97fae859811caa7515ce
VanshVg <vansh.vg18@gmail.com>
2025-05-08 00:11:14 +0530

    Added products card for TopProducts Section

--- src/modules/Customer/pages/Home/components/TopProductsSection.tsx
+++ src/modules/Customer/pages/Home/components/TopProductsSection.tsx
@@ -4,6 +4,32 @@ const TopProductsSection = () => {
       <h1 className="font-primary text-[25px] sm:text-[30px] md:text-[35px] lg:text-[40px] xl:text-[45px] italic">
         Top Products
       </h1>
+      <div className="flex p-4 flex-wrap">
+        <div className="w-[50%] p-3">
+          <div className="border-primary border-[1px] min-h-[250px]">
+            <div className="min-h-[170px] bg-[#edebe7]"></div>
+            <div className="min-h-[80px] bg-primary"></div>
+          </div>
+        </div>
+        <div className="w-[50%] p-3">
+          <div className="border-primary border-[1px] min-h-[250px]">
+            <div className="min-h-[170px] bg-[#edebe7]"></div>
+            <div className="min-h-[80px] bg-primary"></div>
+          </div>
+        </div>
+        <div className="w-[50%] p-3">
+          <div className="border-primary border-[1px] min-h-[250px]">
+            <div className="min-h-[170px] bg-[#edebe7]"></div>
+            <div className="min-h-[80px] bg-primary"></div>
+          </div>
+        </div>
+        <div className="w-[50%] p-3">
+          <div className="border-primary border-[1px] min-h-[250px]">
+            <div className="min-h-[170px] bg-[#edebe7]"></div>
+            <div className="min-h-[80px] bg-primary"></div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
