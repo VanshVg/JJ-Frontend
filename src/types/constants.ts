e8195c8b8f61db858b496d6c79eec63cf767ce98
VanshVg <vansh.vg18@gmail.com>
2025-05-13 20:15:20 +0530

    added static product images

--- src/types/constants.ts
+++ src/types/constants.ts
@@ -1,1 +1,6 @@
 export const logoPath = "./images/logo.png";
+
+export const rupeesSymbol = "₹";
+
+export const testProductFront = "./images/test-product-front.png";
+export const testProductBack = "./images/test-product-back.png";
