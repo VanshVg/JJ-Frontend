adef69840e965930c5b75854278bb8fc4789e03b
VanshVg <vansh.vg18@gmail.com>
2025-05-15 00:44:30 +0530

    completed top products page

---
+++ src/modules/Customer/pages/Home/types/constants.ts
@@ -0,0 +1,47 @@
+import {
+  testProductBack,
+  testProductFront,
+} from "../../../../../types/constants";
+
+export const topProductsData = [
+  {
+    frontImageUrl: testProductFront,
+    backImageUrl: testProductBack,
+    productName: "Product 1",
+  },
+  {
+    frontImageUrl: testProductFront,
+    backImageUrl: testProductBack,
+    productName: "Product 2",
+  },
+  {
+    frontImageUrl: testProductFront,
+    backImageUrl: testProductBack,
+    productName: "Product 3",
+  },
+  {
+    frontImageUrl: testProductFront,
+    backImageUrl: testProductBack,
+    productName: "Product 4",
+  },
+  {
+    frontImageUrl: testProductFront,
+    backImageUrl: testProductBack,
+    productName: "Product 5",
+  },
+  {
+    frontImageUrl: testProductFront,
+    backImageUrl: testProductBack,
+    productName: "Product 6",
+  },
+  {
+    frontImageUrl: testProductFront,
+    backImageUrl: testProductBack,
+    productName: "Product 7",
+  },
+  {
+    frontImageUrl: testProductFront,
+    backImageUrl: testProductBack,
+    productName: "Product 8",
+  },
+];
