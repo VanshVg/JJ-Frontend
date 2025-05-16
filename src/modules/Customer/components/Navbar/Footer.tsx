abb099780f6f1f1f68cadd6580b67e30e20791c1
VanshVg <vansh.vg18@gmail.com>
2025-05-16 17:39:38 +0530

    completed footer

--- src/modules/Customer/components/Navbar/Footer.tsx
+++ src/modules/Customer/components/Navbar/Footer.tsx
@@ -1,28 +1,22 @@
 const Footer = () => {
   return (
-    <footer className="border-t-[2px] border-primary mt-16 max-w-[98%] mx-auto mb-12 text-left">
-      <div className="text-primary mt-2 border-b-[1px]">
-        <div className="border-b-[1px] border-primary p-6 text-center">
-          <h1 className="font-primary font-semibold text-[14px]">Contact Us</h1>
-          <div className="text-[10px] mt-2">
-            <p>+91 9825153013,</p>
+    <footer className="border-t-[2px] border-primary mt-16 mx-auto mb-1 text-left">
+      <div className="text-primary mt-2 border-b-[1px] sm:flex sm:justify-between">
+        <div className="border-b-[1px] border-primary p-4 sm:border-b-[0px] sm:border-r-[1px] sm:w-[50%]">
+          <h1 className="font-primary font-semibold text-[14px] text-center sm:text-[18px] md:text-[22px]">
+            Contact Us
+          </h1>
+          <div className="text-[9px] mt-2 flex justify-center gap-4 sm:text-[14px] md:text-[16px]">
+            <p>+91 9825153013</p>
+            <p>|</p>
             <p>+91 9737353013</p>
           </div>
         </div>
-        <div className="border-b-[1px] border-primary p-6 text-center">
-          <h1 className="font-primary font-semibold text-[14px]">
-            Opening Hours
+        <div className="p-4 text-center sm:border-b-[0px] sm:w-[50%]">
+          <h1 className="font-primary font-semibold text-[14px] sm:text-[18px] md:text-[22px]">
+            Visit Us
           </h1>
-          <div className="text-[10px] mt-2">
-            <p>
-              Mon - Sat<b> :</b> 9am - 7PM
-            </p>
-            <p>Sun: Closed</p>
-          </div>
-        </div>
-        <div className="p-6 text-center">
-          <h1 className="font-primary font-semibold text-[14px]">Visit Us</h1>
-          <div className="text-[10px] mt-2">
+          <div className="text-[9px] mt-2 sm:text-[14px] md:text-[16px]">
             <p>Jay Jalaram Trading Co. behind clock tower,</p>
             <p>Goya Bazar, Ankleshwar - 393001</p>
           </div>
