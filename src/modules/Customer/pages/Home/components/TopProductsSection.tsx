48e707dee1d364428133c814410a764fecd0ec2e
VanshVg <vansh.vg18@gmail.com>
2025-05-17 12:07:12 +0530

    resolved build issue

--- src/modules/Customer/pages/Home/components/TopProductsSection.tsx
+++ src/modules/Customer/pages/Home/components/TopProductsSection.tsx
@@ -1,10 +1,6 @@
-import { useNavigate } from "react-router-dom";
 import { topProductsData } from "../types/constants";
-import { ICustomerRoutes } from "../../../types";
 
 const TopProductsSection = () => {
-  const navigate = useNavigate();
-
   return (
     <section className="text-primary mt-[45px] sm:mt-[70px] mb-[100px]">
       <h1 className="font-primary text-[25px] sm:text-[30px] md:text-[35px] lg:text-[40px] xl:text-[45px] italic">
