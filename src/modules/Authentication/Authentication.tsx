556564bdf8c93ad8408d57d60e3f58f89c5a3bdd
VanshVg <vansh.vg18@gmail.com>
2025-05-25 15:57:24 +0530

    added routes guard

--- src/modules/Authentication/Authentication.tsx
+++ src/modules/Authentication/Authentication.tsx
@@ -1,6 +1,15 @@
-import { Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { Navigate, Outlet } from "react-router-dom";
+import { getAuth } from "../../redux/slices/auth.slice";
+import { ICustomerRoutes } from "../Customer/types";
 
 const Authentication = () => {
+  const { isAuthenticated } = useSelector(getAuth);
+
+  if (isAuthenticated) {
+    return <Navigate to={ICustomerRoutes.Home} />;
+  }
+
   return <Outlet />;
 };
 
--- src/modules/Customer/pages/Account/Account.tsx
+++ src/modules/Customer/pages/Account/Account.tsx
@@ -1,4 +1,15 @@
+import { useSelector } from "react-redux";
+import { getAuth } from "../../../../redux/slices/auth.slice";
+import { Navigate } from "react-router-dom";
+import { IAuthenticationRoutes } from "../../../Authentication/types";
+
 const Account = () => {
+  const { isAuthenticated } = useSelector(getAuth);
+
+  if (!isAuthenticated) {
+    return <Navigate to={IAuthenticationRoutes.Login} />;
+  }
+
   return (
     <div className="mt-6">
       <h1 className="font-primary text-[35px]">Account Page</h1>
