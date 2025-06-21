a29414a12e70beaaa6e95f0cb0d6757d279f53ce
VanshVg <vansh.vg18@gmail.com>
2025-06-21 12:32:52 +0530

    Added product module

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,13 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import customerRoutes from "./modules/Customer/Routes";
 import authenticationRoutes from "./modules/Authentication/Routes";
+import productRoutes from "./modules/Product/Routes";
 
 const Router = () => {
   const router = createBrowserRouter([
     ...customerRoutes,
     ...authenticationRoutes,
+    ...productRoutes,
   ]);
 
   return <RouterProvider router={router} />;
---
+++ src/modules/Product/types/index.ts
@@ -0,0 +1,3 @@
+export enum IProductRoute {
+  Product = "/product/:id",
+}
